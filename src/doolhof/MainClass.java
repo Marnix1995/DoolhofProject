86dc95ea80875e0e227fd45d3b39adbf452c19b9
Marnix1995 <marnixn04@gmail.com>
2015-05-30 14:41:22 +0200

    Basis voor onze doolhof.

---
+++ src/doolhof/MainClass.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doolhof;
+
+/**
+ *
+ * @author Marnix
+ */
+
+public class MainClass {
+
+  
+    public static void main(String[] args) {
+
+        Doolhof doolhof = new Doolhof(); 
+        doolhof.Doolhof();
+    }
+   
+}
