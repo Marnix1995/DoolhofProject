2a1ddb367c42c47ae383de28825f4059ee6de0ec
Marnix1995 <marnixn04@gmail.com>
2015-05-30 14:42:28 +0200

    Basis voor onze doolhof.

---
+++ src/doolhof/KeyBoard.java
@@ -0,0 +1,20 @@
+package doolhof;
+
+import java.awt.event.KeyEvent;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marnix
+ */
+
+public interface KeyBoard {
+   
+    
+  public void keyPressed(KeyEvent e); 
+     
+}
