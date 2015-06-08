7b3482e03c90786473d135d928571ecf2a55f1f2
Marnix1995 <marnixn04@gmail.com>
2015-06-08 14:31:15 +0200

    Timer haalt tijd netjes af.

--- src/doolhof/Pacman.java
+++ src/doolhof/Pacman.java
@@ -47,7 +47,7 @@ public class Pacman extends SpelObject {
             }
         else
         {
-            boolean heeftMunitie = false;
+             heeftMunitie = false;
         }
             
     }
@@ -97,7 +97,7 @@ public class Pacman extends SpelObject {
            
             if (!(item instanceof Muur)) {
 
-                boolean heeftMunitie = false;
+                heeftMunitie = false;
                 munitie ++;
                 vak.verplaats(target);
                 vak = target;
