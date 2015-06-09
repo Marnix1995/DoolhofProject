bbf2f1c66596b2e198ef1acf5feeed63819a4f32
alois28 <alois2828@gmail.com>
2015-06-09 22:06:55 +0200

    schieten aanpassing

--- src/doolhof/Pacman.java
+++ src/doolhof/Pacman.java
@@ -19,7 +19,7 @@ public class Pacman extends SpelObject {
     private Image imgBoven = new ImageIcon(Pacman.class.getResource("\\Plaatjes\\playerBoven.png")).getImage();
     private Image imgOnder = new ImageIcon(Pacman.class.getResource("\\Plaatjes\\playerOnder.png")).getImage();
     private int dir;
-    private int munitie = 45;
+    private int munitie = 1;
     boolean heeftMunitie = false;
     private int x;
     private int y;
@@ -32,9 +32,6 @@ public class Pacman extends SpelObject {
         return store_kogelOp;
     }
 
-    public void setBULLETS(Kogel value) {
-        store_kogelOp.add(value);
-    }
 
     public void schiet() {
         if (heeftMunitie = true && (munitie > 0)) {
@@ -107,6 +104,7 @@ public class Pacman extends SpelObject {
                 
                 heeftMunitie = false;                
                 vak.level.timer.setMunitie(munitie);
+                munitie++;
             }
             if (item instanceof Helper) {
                 
